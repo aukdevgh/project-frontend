62d97147cc9898dbea7d7324ef871f2c719a1f68
Ulan <aukdevgm@gmail.com>
2024-12-17 01:01:37 +0600

    build(app): add eslint-config-prettier eslint-plugin-import

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,77 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      // js
+      js.configs.recommended,
+      // ts
+      ...tseslint.configs.recommended,
+      // react
+      react.configs.flat.recommended,
+      // import
+      importPlugin.flatConfigs.recommended,
+      // prettier
+      eslintConfigPrettier,
+    ],
+
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+
+    // specify used plugins
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+
+    settings: {
+      // for eslint-plugin-react to auto detect react version
+      react: {
+        version: 'detect',
+      },
+      // for eslint-plugin-import to use import alias
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+        alias: {
+          map: [
+            ['', './public'], // <-- this line
+          ],
+          extensions: ['.ts', '.tsx'],
+        },
+      },
+    },
+
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-console': 'warn',
+      'react/button-has-type': 'error',
+      'react/react-in-jsx-scope': ['off'],
+
+      'import/order': [
+        'error',
+        {
+          groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling'], ['index']],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
   }
 )
