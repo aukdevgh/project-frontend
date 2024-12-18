fd6811639094584b7ba25a167331f3597d182027
Ulan <aukdevgm@gmail.com>
2024-12-19 00:02:01 +0600

    build(app): add stylelint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,11 @@
 export default {
   '**/*.{ts,tsx}': (filenames) => [
-    `eslint --cache --cache-location ./node_modules/.cache/.eslintcache ${filenames.join(' ')}`,
+    `eslint --cache --cache-location ./node_modules/.cache/.eslintcache --fix ${filenames.join(' ')}`,
     `prettier  --loglevel warn --cach --write ${filenames.join(' ')}`,
   ],
+  '*.scss': (filenames) => [
+    `stylelint --cache --cache-location ./node_modules/.cache/.stylelintcache --fix ${filenames.join(' ')}`,
+    `prettier  --loglevel warn --cach --write ${filenames.join(' ')}`,
+  ],
+  '*.{json,yml}': (filenames) => `prettier  --loglevel warn --cach --write ${filenames.join(' ')}`,
 }
