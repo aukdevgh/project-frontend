daae7537b77512bc034601eb913fc41ef0dc4cd2
Ulan <aukdevgm@gmail.com>
2024-12-20 19:11:50 +0600

    build(app): add vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { resolve } from 'path'
 
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr({ include: '**/*.svg' })],
 
   resolve: {
     alias: {
